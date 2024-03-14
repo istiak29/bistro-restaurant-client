df80684eadf6a65ca8072147b199274c2b2a45dd
Istiak Islam <tistiak@outlook.com>
2024-03-14 13:22:46 +0600

    add banner

--- src/Pages/HomePage/Banner/Banner.jsx
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -1,19 +1,43 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
+import bannerImage1 from "../../../assets/home/01.jpg";
+import bannerImage2 from "../../../assets/home/02.jpg";
+import bannerImage3 from "../../../assets/home/03.png";
+import bannerImage4 from "../../../assets/home/04.jpg";
+import bannerImage5 from "../../../assets/home/05.png";
+import bannerImage6 from "../../../assets/home/06.png";
+
 const Banner = () => {
     return (
         <Carousel>
             <div>
-                <img src="assets/1.jpeg" />
+                <img src={bannerImage1} />
                 <p className="legend">Legend 1</p>
             </div>
+
             <div>
-                <img src="assets/2.jpeg" />
+                <img src={bannerImage2} />
                 <p className="legend">Legend 2</p>
             </div>
+
             <div>
-                <img src="assets/3.jpeg" />
+                <img src={bannerImage3} />
+                <p className="legend">Legend 3</p>
+            </div>
+            
+            <div>
+                <img src={bannerImage4} />
+                <p className="legend">Legend 3</p>
+            </div>
+            
+            <div>
+                <img src={bannerImage5} />
+                <p className="legend">Legend 3</p>
+            </div>
+            
+            <div>
+                <img src={bannerImage6} />
                 <p className="legend">Legend 3</p>
             </div>
         </Carousel>
--- src/Pages/HomePage/Home/Home.jsx
+++ src/Pages/HomePage/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
-            This is my homeeeeeeeee
+           <Banner></Banner>
         </div>
     );
 };
